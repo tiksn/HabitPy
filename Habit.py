5cd30baad72f130a3fb620d4e32e277fd953474c
Tigran Torosyan <tiksn@localhost.localdomain>
2019-01-22 05:39:32 +0200

    base script added

---
+++ Habit.py
@@ -0,0 +1,9 @@
+from habitipy import Habitipy, load_conf,DEFAULT_CONF
+conf = load_conf(DEFAULT_CONF)
+api = Habitipy(conf)
+
+
+for tag in api.tags.get():
+    print(tag)
+
+importantTagId = conf["Important"]
