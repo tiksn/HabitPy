18480fef00a83096bcf9f86128ed39f5e0bcfabf
Tigran Torosyan <tiksn@localhost.localdomain>
2019-01-22 06:37:38 +0200

    print backlog

--- Habit.py
+++ Habit.py
@@ -6,3 +6,14 @@ importantTagId = conf.get("Important")
 urgentTagId = conf.get("Urgent")
 lessImportantTagId = conf.get("LessImportant")
 lessUrgentTagId = conf.get("LessUrgent")
+
+def processToDos(selector, processor):
+    todos = api.tasks.user.get(type = "todos")
+    for todo in todos:
+        tags = todo["tags"]
+        isImportant = True if importantTagId in tags else False
+        isUrgent = True if urgentTagId in tags else False
+        isLessImportant = True if lessImportantTagId in tags else False
+        isLessUrgent = True if lessUrgentTagId in tags else False
+        if selector(todo, isImportant, isUrgent, isLessImportant, isLessUrgent):
+            processor(todo, isImportant, isUrgent, isLessImportant, isLessUrgent)
---
+++ Prioritize.py
@@ -0,0 +1,4 @@
+from Habit import *
+
+
+listToDos()
