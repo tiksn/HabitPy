84fed1e1f191661415ee9afa09e8a3236b2c8ec6
Tigran Torosyan <tiksn@localhost.localdomain>
2019-01-22 06:59:33 +0200

    partial prioritization

--- Prioritize.py
+++ Prioritize.py
@@ -1,8 +1,16 @@
 from Habit import *
-
+import str2bool as s2b
 
 def isBacklogItem(todo, isImportant, isUrgent, isLessImportant, isLessUrgent):
     return (isImportant == False) and (isUrgent == False) and (isLessImportant == False) and (isLessUrgent == False)
 
+def prioritizeTodo(todo, isImportant, isUrgent, isLessImportant, isLessUrgent):
+    printTodo(todo, isImportant, isUrgent, isLessImportant, isLessUrgent)
+    markAsImportant = s2b.str2bool_exc(input("Is this to-do important? "))
+    markAsUrgent = s2b.str2bool_exc(input("Is this to-do urgent? "))
 
-processToDos(isBacklogItem, printTodo)
+    importancyTagId = importantTagId if markAsImportant else lessImportantTagId
+    urgencyTagId = urgentTagId if markAsUrgent else lessUrgentTagId
+
+    print(dir(todo["tags"]))
+processToDos(isBacklogItem, prioritizeTodo)
