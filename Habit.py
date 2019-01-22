fa8208cb0f9bf0e52e7f731050bf1cbfa63d457d
Tigran Torosyan <tiksn@localhost.localdomain>
2019-01-22 05:45:57 +0200

    get tags

--- Habit.py
+++ Habit.py
@@ -2,8 +2,7 @@ from habitipy import Habitipy, load_conf,DEFAULT_CONF
 conf = load_conf(DEFAULT_CONF)
 api = Habitipy(conf)
 
-
-for tag in api.tags.get():
-    print(tag)
-
-importantTagId = conf["Important"]
+importantTagId = conf.get("Important")
+urgentTagId = conf.get("Urgent")
+lessImportantTagId = conf.get("LessImportant")
+lessUrgentTagId = conf.get("LessUrgent")
