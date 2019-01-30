ead5f0a52c6549d20a5a430e6c54b34f82ab8d82
Tigran Torosyan <tiksn@localhost.localdomain>
2019-01-30 02:44:59 +0200

    conditionals

--- Prioritize.py
+++ Prioritize.py
@@ -9,8 +9,27 @@ def prioritizeTodo(todo, isImportant, isUrgent, isLessImportant, isLessUrgent):
     markAsImportant = s2b.str2bool_exc(input("Is this to-do important? "))
     markAsUrgent = s2b.str2bool_exc(input("Is this to-do urgent? "))
 
-    importancyTagId = importantTagId if markAsImportant else lessImportantTagId
-    urgencyTagId = urgentTagId if markAsUrgent else lessUrgentTagId
+    tagsToRemove = []
+    tagsToAdd = []
 
-    print(dir(todo["tags"]))
+    if isImportant and (not markAsImportant):
+        tagsToRemove.append(importantTagId)
+    if isLessImportant and markAsImportant:
+        tagsToRemove.append(lessImportantTagId)
+    if isUrgent and (not markAsUrgent):
+        tagsToRemove.append(urgentTagId)
+    if isLessUrgent and markAsUrgent:
+        tagsToRemove.append(lessUrgentTagId)
+    
+    if markAsImportant and (not isImportant):
+        tagsToAdd.append(importantTagId)
+    if (not markAsImportant) and (not isLessImportant):
+        tagsToAdd.append(lessImportantTagId)
+    if markAsUrgent and (not isUrgent):
+        tagsToAdd.append(urgentTagId)
+    if (not markAsUrgent) and (not isLessUrgent):
+        tagsToAdd.append(lessUrgentTagId)
+    
+    print(tagsToRemove)
+    print(tagsToAdd)
 processToDos(isBacklogItem, prioritizeTodo)
